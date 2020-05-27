c8f33534d1ff84f4295bcbd0a7ed31bdb266181c
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-05-27 16:57:19 -0400

    UI: Reusable: add a reusable CustomAlert

---
+++ lib/ui/reusable/alertdialog.dart
@@ -0,0 +1,36 @@
+// Flutter
+import 'package:flutter/material.dart';
+
+class CustomAlert extends StatelessWidget {
+  final Icon leadingIcon;
+  final String title;
+  final String content;
+  final List<Widget> actions;
+  CustomAlert({
+    this.leadingIcon,
+    @required this.title,
+    @required this.content,
+    @required this.actions
+  });
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ), 
+      title: Row(
+        children: <Widget>[
+          leadingIcon,
+          SizedBox(width: 6),
+          Text(title, style: TextStyle(
+            color: Theme.of(context).textTheme.body1.color
+          )),
+        ],
+      ),
+      content: Text(content, style: TextStyle(
+        color: Theme.of(context).textTheme.body1.color
+      )),
+      actions: actions
+    );
+  }
+}
