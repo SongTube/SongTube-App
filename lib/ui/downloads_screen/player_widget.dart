8d1ac98fea30cdb4b615eb6a6c925a23682679b9
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-06-16 06:36:45 -0400

    UI: Player Widget: fix FullPlayerWidget UI

--- lib/ui/downloads_screen/player_widget.dart
+++ lib/ui/downloads_screen/player_widget.dart
@@ -8,10 +8,13 @@ import 'package:flutter/material.dart';
 
 // Internal
 import 'package:songtube/internal/player_service.dart';
+import 'package:songtube/provider/downloads_manager.dart';
 
 // Packages
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:audio_service/audio_service.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:provider/provider.dart';
 
 class PlayerWidget extends StatefulWidget {
   final Function onPlayPauseTap;
@@ -91,68 +94,143 @@ class FullPlayerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: StreamBuilder<ScreenState>(
-        stream: _screenStateStream,
+    ManagerProvider manager = Provider.of<ManagerProvider>(context);
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height - kToolbarHeight*1.5 - kBottomNavigationBarHeight,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Theme.of(context).cardColor,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0.0, 0.0), //(x,y)
+            blurRadius: 10.0,
+            spreadRadius: 5.0
+          ),
+        ],
+      ),
+    child: StreamBuilder<ScreenState>(
+      stream: _screenStateStream,
         builder: (context, snapshot) {
           final screenState = snapshot.data;
-          final queue = screenState?.queue;
           final mediaItem = screenState?.mediaItem;
           final state = screenState?.playbackState;
           final processingState =
               state?.processingState ?? AudioProcessingState.none;
           final playing = state?.playing ?? false;
           return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               if (processingState == AudioProcessingState.none) ...[
-                audioPlayerButton(),
+                Text("No audio in Queue")
               ] else ...[
-                if (queue != null && queue.isNotEmpty)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.skip_previous),
-                        iconSize: 64.0,
-                        onPressed: mediaItem == queue.first
-                            ? null
-                            : AudioService.skipToPrevious,
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.skip_next),
-                        iconSize: 64.0,
-                        onPressed: mediaItem == queue.last
-                            ? null
-                            : AudioService.skipToNext,
-                      ),
-                    ],
+                Container(
+                  height: 320,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12.withOpacity(0.2),
+                        offset: Offset(0, 3), //(x,y)
+                        blurRadius: 6.0,
+                        spreadRadius: 1.0 
+                      )
+                    ]
                   ),
-                if (mediaItem?.title != null) Text(mediaItem.title),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    if (playing) pauseButton() else playButton(),
-                    stopButton(),
-                  ],
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: FadeInImage(
+                      fadeOutDuration: Duration(milliseconds: 300),
+                      fadeInDuration: Duration(milliseconds: 300),
+                      placeholder: MemoryImage(kTransparentImage),
+                      image: NetworkImage(mediaItem.artUri),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
                 positionIndicator(mediaItem, state),
-                Text("Processing state: " +
-                    "$processingState".replaceAll(RegExp(r'^.*\.'), '')),
-                StreamBuilder(
-                  stream: AudioService.customEventStream,
-                  builder: (context, snapshot) {
-                    return Text("custom event: ${snapshot.data}");
-                  },
+                Column(
+                  children: <Widget>[
+                    // Title
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8, right: 8),
+                      child: Text(
+                        mediaItem.title,
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: "Varela",
+                          color: Theme.of(context).textTheme.bodyText1.color,
+                        ),
+                        textAlign: TextAlign.center,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    // Artist
+                    Text(
+                      mediaItem.artist,
+                      style: TextStyle(color: Theme.of(context).iconTheme.color, fontFamily: "Varela")
+                    ),
+                  ],
                 ),
-                StreamBuilder<bool>(
-                  stream: AudioService.notificationClickEventStream,
-                  builder: (context, snapshot) {
-                    return Text(
-                      'Notification Click Status: ${snapshot.data}',
-                    );
-                  },
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    // Previous button
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios, color: Theme.of(context).iconTheme.color),
+                      onPressed: () => AudioService.skipToNext(),
+                    ),
+                    // Padding
+                    SizedBox(width: 30),
+                    // Play/Pause button
+                    GestureDetector(
+                      onTap: playing
+                        ? () => AudioService.pause()
+                        : () => AudioService.play(),
+                      child: Container(
+                        height: 60,
+                        width: 60,
+                        padding: EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          color: Colors.redAccent,
+                          borderRadius: BorderRadius.circular(40),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12.withOpacity(0.2),
+                                offset: Offset(0, 3), //(x,y)
+                                blurRadius: 6.0,
+                                spreadRadius: 1.0 
+                              )
+                            ]
+                        ),
+                        child: playing
+                          ? Icon(Icons.pause, size: 35, color: Colors.white)
+                          : Icon(Icons.play_arrow, size: 35, color: Colors.white),
+                      ),
+                    ),
+                    // Padding
+                    SizedBox(width: 30),
+                    // Next button
+                    IconButton(
+                      icon: Icon(Icons.arrow_forward_ios, color: Theme.of(context).iconTheme.color),
+                      onPressed: () => AudioService.skipToNext(),
+                    )
+                  ],
                 ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  padding: EdgeInsets.only(right: 12),
+                  child: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () => manager.showMediaPlayer = false,
+                  ),
+                )
               ],
             ],
           );
@@ -171,45 +249,6 @@ class FullPlayerWidget extends StatelessWidget {
           (queue, mediaItem, playbackState) =>
               ScreenState(queue, mediaItem, playbackState));
 
-  RaisedButton audioPlayerButton() => startButton(
-        'AudioPlayer',
-        () {
-          AudioService.start(
-            backgroundTaskEntrypoint: audioPlayerTaskEntrypoint,
-            androidNotificationChannelName: 'SongTube',
-            // Enable this if you want the Android service to exit the foreground state on pause.
-            //androidStopForegroundOnPause: true,
-            androidNotificationColor: 0xFF2196f3,
-            androidNotificationIcon: 'mipmap/ic_stat_music_note',
-            androidEnableQueue: true,
-          );
-        },
-      );
-
-  RaisedButton startButton(String label, VoidCallback onPressed) =>
-      RaisedButton(
-        child: Text(label),
-        onPressed: onPressed,
-      );
-
-  IconButton playButton() => IconButton(
-        icon: Icon(Icons.play_arrow),
-        iconSize: 64.0,
-        onPressed: AudioService.play,
-      );
-
-  IconButton pauseButton() => IconButton(
-        icon: Icon(Icons.pause),
-        iconSize: 64.0,
-        onPressed: AudioService.pause,
-      );
-
-  IconButton stopButton() => IconButton(
-        icon: Icon(Icons.stop),
-        iconSize: 64.0,
-        onPressed: AudioService.stop,
-      );
-
   Widget positionIndicator(MediaItem mediaItem, PlaybackState state) {
     double seekPos;
     return StreamBuilder(
@@ -218,34 +257,72 @@ class FullPlayerWidget extends StatelessWidget {
           Stream.periodic(Duration(milliseconds: 200)),
           (dragPosition, _) => dragPosition),
       builder: (context, snapshot) {
-        double position =
-            snapshot.data ?? state.currentPosition.inMilliseconds.toDouble();
-        double duration = mediaItem?.duration?.inMilliseconds?.toDouble();
-        return Column(
-          children: [
-            if (duration != null)
-              Slider(
-                min: 0.0,
-                max: duration,
-                value: seekPos ?? max(0.0, min(position, duration)),
-                onChanged: (value) {
-                  _dragPositionSubject.add(value);
-                },
-                onChangeEnd: (value) {
-                  AudioService.seekTo(Duration(milliseconds: value.toInt()));
-                  // Due to a delay in platform channel communication, there is
-                  // a brief moment after releasing the Slider thumb before the
-                  // new position is broadcast from the platform side. This
-                  // hack is to hold onto seekPos until the next state update
-                  // comes through.
-                  // TODO: Improve this code.
-                  seekPos = value;
-                  _dragPositionSubject.add(null);
-                },
+        Duration position = snapshot.data ?? state.currentPosition;
+        Duration duration = mediaItem?.duration;
+        return duration != null
+          ? Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 16, right: 16),
+                child: SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 0),
+                    overlayShape: RoundSliderOverlayShape(overlayRadius: 10),
+                    valueIndicatorTextStyle: TextStyle(
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                  child: Slider(
+                    activeColor: Colors.redAccent,
+                    inactiveColor: Colors.black12.withOpacity(0.2),
+                    min: 0.0,
+                    max: duration.inMilliseconds?.toDouble(),
+                    value: seekPos ?? max(0.0, min(
+                      position.inMilliseconds.toDouble(),
+                      duration.inMilliseconds?.toDouble()
+                    )),
+                    onChanged: (value) {
+                      _dragPositionSubject.add(value);
+                    },
+                    onChangeEnd: (value) {
+                      AudioService.seekTo(Duration(milliseconds: value.toInt()));
+                      // Due to a delay in platform channel communication, there is
+                      // a brief moment after releasing the Slider thumb before the
+                      // new position is broadcast from the platform side. This
+                      // hack is to hold onto seekPos until the next state update
+                      // comes through.
+                      // TODO: Improve this code.
+                      seekPos = value;
+                      _dragPositionSubject.add(null);
+                    },
+                  )
+                ),
               ),
-            Text("${state.currentPosition}"),
-          ],
-        );
+              Padding(
+                padding: EdgeInsets.only(left: 24, right: 24),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      "${position.inMinutes}:${(position.inSeconds.remainder(60).toString().padLeft(2, '0'))}",
+                      style: TextStyle(
+                        fontFamily: "Varela",
+                        fontSize: 12,
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      "${duration.inMinutes}:${(duration.inSeconds.remainder(60).toString().padLeft(2, '0'))}",
+                      style: TextStyle(
+                        fontFamily: "Varela",
+                        fontSize: 12,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+          : Container();
       },
     );
   }
