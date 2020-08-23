50dcfb1dff5a1199810acfae2bf7b62f63ad1e3e
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-08-23 08:11:59 -0400

    Code cleanup

--- lib/screens/homeScreen/searchPage.dart
+++ lib/screens/homeScreen/searchPage.dart
@@ -2,50 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:songtube/ui/searchResult/videoTile.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
-class SearchPage extends StatefulWidget {
+class SearchPage extends StatelessWidget {
   final List results;
   final Function onSelect;
   SearchPage({
     @required this.results,
     @required this.onSelect,
   });
-
-  @override
-  _SearchPageState createState() => _SearchPageState();
-}
-
-class _SearchPageState extends State<SearchPage> with TickerProviderStateMixin {
-
-  // Youtube Explode Client
-  YoutubeExplode client;
-
-  @override
-  void initState() {
-    super.initState();
-    client = new YoutubeExplode();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    client.close();
-  }
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       physics: BouncingScrollPhysics(),
-      itemCount: widget.results.length,
+      itemCount: results.length,
       itemBuilder: (context, index) {
         return Padding(
           padding: EdgeInsets.only(
-            top: widget.results[index] == widget.results.first ? 10 : 0,
-            bottom: widget.results[index] == widget.results.last ? 10 : 0
+            top: results[index] == results.first ? 10 : 0,
+            bottom: results[index] == results.last ? 10 : 0
           ),
           child: VideoTile(
-            client: client,
-            video: widget.results[index],
-            onSelect: widget.onSelect,
+            client: YoutubeExplode(),
+            video: results[index],
+            onSelect: onSelect,
           ),
         );
       }
--- lib/ui/searchResult/videoTile.dart
+++ lib/ui/searchResult/videoTile.dart
@@ -5,9 +5,8 @@ import 'package:provider/provider.dart';
 import 'package:songtube/provider/managerProvider.dart';
 import 'package:transparent_image/transparent_image.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart' as yt;
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class VideoTile extends StatefulWidget {
+class VideoTile extends StatelessWidget {
   final yt.SearchVideo video;
   final yt.YoutubeExplode client;
   final Function onSelect;
@@ -17,149 +16,106 @@ class VideoTile extends StatefulWidget {
     @required this.onSelect
   });
   @override
-  _VideoTileState createState() => _VideoTileState();
-}
-
-class _VideoTileState extends State<VideoTile> with TickerProviderStateMixin{
-
-  // Show Detials
-  bool loadVideo = false;
-
-  // Logo URL
-  String logoUrl;
-
-  // Get logo url
-  void getLogoUrl() async {
-    try {
-      await widget.client.channels.
-        getByVideo(widget.video.videoId).then((value) => {
-          if (mounted) {
-            setState(() => logoUrl = value.logoUrl)
-          }
-      });
-    } catch (_) {}
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    try {
-      getLogoUrl();
-    } catch (_) {}
-  }
-
-  @override
   Widget build(BuildContext context) {
     ManagerProvider manager = Provider.of<ManagerProvider>(context);
-    return Ink(
-      height: 310,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Theme.of(context).cardColor,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12.withOpacity(0.05),
-            offset: Offset(0, 3), //(x,y)
-            blurRadius: 6.0,
-            spreadRadius: 0.01 
-          )
-        ]
-      ),
-      child: Column(
-        children: <Widget>[
-          GestureDetector(
-            onTap: () => setState(() => loadVideo = true),
-            child: Container(
+    return InkWell(
+      onTap: () => manager
+        .getVideoDetails("https://www.youtube.com/watch?v=${video.videoId}"),
+      child: Ink(
+        height: 310,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12.withOpacity(0.05),
+              offset: Offset(0, 3), //(x,y)
+              blurRadius: 6.0,
+              spreadRadius: 0.01 
+            )
+          ]
+        ),
+        child: Column(
+          children: <Widget>[
+            Container(
               height: 230,
               width: double.infinity,
-              child: loadVideo
-                ? YoutubePlayer(
-                    controller: YoutubePlayerController(
-                      initialVideoId: widget.video.videoId.value,
-                      flags: YoutubePlayerFlags(
-                        autoPlay: true,
-                        mute: false,
-                      )
-                    ),
-                  )
-                : FadeInImage(
-                    fadeInDuration: Duration(milliseconds: 200),
-                    placeholder: MemoryImage(kTransparentImage),
-                    image: NetworkImage("http://img.youtube.com/vi/${widget.video.videoId}/hqdefault.jpg"),
-                    fit: BoxFit.fitWidth,
-                  )
+              child: FadeInImage(
+                fadeInDuration: Duration(milliseconds: 200),
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage("http://img.youtube.com/vi/${video.videoId}/hqdefault.jpg"),
+                fit: BoxFit.fitWidth,
+              )
             ),
-          ),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(left: 8),
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: FadeInImage(
-                      fadeInDuration: Duration(milliseconds: 150),
-                      image: logoUrl == null
-                        ? AssetImage('assets/images/grey.jpg')
-                        : NetworkImage(logoUrl),
-                      placeholder: MemoryImage(kTransparentImage),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(left: 8),
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
                     ),
-                  )
-                ),
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.only(left: 8, right: 8),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "${widget.video.videoTitle}",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: FutureBuilder(
+                        future: client.channels.getByVideo(video.videoId),
+                        builder: (context, AsyncSnapshot<yt.Channel> result) {
+                          if (result.hasData) {
+                            return Image.network(result.data.logoUrl);
+                          } else {
+                            return Image.asset('assets/images/grey.jpg');
+                          }
+                        },
+                      )
+                    )
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 8, right: 8),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "${video.videoTitle}",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500
+                            ),
+                            overflow: TextOverflow.clip,
+                            maxLines: 1,
                           ),
-                          overflow: TextOverflow.clip,
-                          maxLines: 1,
-                        ),
-                        SizedBox(height: 4),
-                        Text(
-                          "${widget.video.videoAuthor} • "
-                          "${NumberFormat.compact().format(widget.video.videoViewCount)} views",
-                          style: TextStyle(
-                            color: Colors.grey[600],
-                            fontSize: 12
+                          SizedBox(height: 4),
+                          Text(
+                            "${video.videoAuthor} • "
+                            "${NumberFormat.compact().format(video.videoViewCount)} views",
+                            style: TextStyle(
+                              color: Colors.grey[600],
+                              fontSize: 12
+                            ),
+                            overflow: TextOverflow.clip,
+                            maxLines: 1,
                           ),
-                          overflow: TextOverflow.clip,
-                          maxLines: 1,
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    manager.getVideoDetails("https://www.youtube.com/watch?v=${widget.video.videoId}");
-                  },
-                  child: Container(
+                  Container(
                     height: 30,
                     width: 30,
                     margin: EdgeInsets.all(16),
                     color: Theme.of(context).cardColor,
                     child: Icon(EvaIcons.downloadOutline, size: 20, color: Theme.of(context).iconTheme.color),
                   ),
-                )
-              ],
+                ],
+              ),
             ),
-          ),
-          Divider(height: 1, thickness: 2)
-        ],
+            Divider(height: 1, thickness: 2)
+          ],
+        ),
       ),
     );
   }
