be0b18a11e55d2dd2fde4dc3b635680ba57dbe14
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-08-27 18:57:40 -0400

    Screens: Downloads: DownloadsEmpty widget rework

--- lib/screens/downloadsPages/downloadsEmpty.dart
+++ lib/screens/downloadsPages/downloadsEmpty.dart
@@ -1,30 +1,44 @@
+// Flutter
 import 'package:flutter/material.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+// Packages
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 
 class NoDownloads extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(8),
+      height: 240,
       width: MediaQuery.of(context).size.width*0.6,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
+        color: Theme.of(context).cardColor,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12.withOpacity(0.08),
+            offset: Offset(0,0),
+            spreadRadius: 0.01,
+            blurRadius: 20.0
+          )
+        ]
       ),
       child: Padding(
-        padding: const EdgeInsets.all(40.0),
+        padding: const EdgeInsets.all(24.0),
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Icon(MdiIcons.cloudDownload, size: 100, color: Theme.of(context).accentColor),
+            Icon(EvaIcons.cloudDownloadOutline, size: 100, color: Theme.of(context).accentColor),
+            SizedBox(height: 8),
             Text(
-              "No downloads yet!",
-              style: TextStyle(fontWeight: FontWeight.w700),
+              "No downloads yet",
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Varela'
+              ),
             ),
             SizedBox(height: 4),
             Text("All your ongoing downloads will be shown here", textAlign: TextAlign.center),
-            SizedBox(height: 120),
           ],
         ),
       ),
