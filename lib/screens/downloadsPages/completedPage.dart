0ba042b238544c9eeb2162c85787ee0183ba575a
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-09-02 00:35:28 -0400

    Downloads: Get MediaItemList from new Function

--- lib/screens/downloadsPages/completedPage.dart
+++ lib/screens/downloadsPages/completedPage.dart
@@ -82,7 +82,7 @@ class _CompletedPageState extends State<CompletedPage> with TickerProviderStateM
                                 androidEnableQueue: true,
                               );
                             }
-                            await AudioService.updateQueue(manager.serviceQueue);
+                            await AudioService.updateQueue(manager.getCurrentMediaItemList());
                             MediaItem item = AudioService.queue[index];
                             await AudioService.playMediaItem(item);
                           }
