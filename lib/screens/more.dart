bc1c195ab879c08094d2b820325e6f8232d12c06
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-09-14 16:30:16 -0400

    Screens: More: Add BouncingScrollPhysics

--- lib/screens/more.dart
+++ lib/screens/more.dart
@@ -14,6 +14,7 @@ class MoreScreen extends StatelessWidget {
     AppDataProvider appData = Provider.of<AppDataProvider>(context);
     return Scaffold(
       body: ListView(
+        physics: BouncingScrollPhysics(),
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(28),
