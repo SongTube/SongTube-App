df15c35179b90e46665d25e9b6a1223f7773ba93
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-09-14 16:22:09 -0400

    UI: SnackBar: Fix snackbar position

--- lib/ui/snackbar.dart
+++ lib/ui/snackbar.dart
@@ -1,5 +1,8 @@
 // Flutter
+import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
+import 'package:songtube/internal/playerService.dart';
+import 'package:songtube/screens/musicPlayer/screenStateStream.dart';
 
 class AppSnack {
 
@@ -19,35 +22,56 @@ class AppSnack {
   }) {
     scaffoldKey.currentState.removeCurrentSnackBar();
     final snack = SnackBar(
-      content: Row(
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Icon(
-            icon,
-            color: Theme.of(context).iconTheme.color,
-          ),
-          SizedBox(width: 8),
-          Column(
+        children: [
+          Row(
             mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              SizedBox(height: 4),
-              Text(
-                title,
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  color: Theme.of(context).textTheme.bodyText1.color
-                )
+              Icon(
+                icon,
+                color: Theme.of(context).iconTheme.color,
               ),
-              if (message != null)
-              Text(
-                message,
-                style: TextStyle(
-                  color: Theme.of(context).textTheme.bodyText1.color
-                )
+              SizedBox(width: 8),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 4),
+                  Text(
+                    title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: Theme.of(context).textTheme.bodyText1.color
+                    )
+                  ),
+                  if (message != null)
+                  Text(
+                    message,
+                    style: TextStyle(
+                      color: Theme.of(context).textTheme.bodyText1.color
+                    )
+                  ),
+                  SizedBox(height: 4),
+                ],
               ),
-              SizedBox(height: 4),
             ],
+          ),
+          StreamBuilder<ScreenState>(
+            stream: screenStateStream,
+            builder: (context, snapshot) {
+              final screenState = snapshot.data;
+              final state = screenState?.playbackState;
+              final processingState =
+                state?.processingState ?? AudioProcessingState.none;
+              return Container(
+                height: processingState != AudioProcessingState.none
+                  ? kToolbarHeight * 1.15
+                  : 0
+              );
+            }
           ),
         ],
       ),
