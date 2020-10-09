f8ca007dffecf0986c642587290a25f7c3109088
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-10-09 19:41:42 -0400

    Screens: Media: remove useless code

--- lib/screens/media.dart
+++ lib/screens/media.dart
@@ -19,97 +19,75 @@ class MediaScreen extends StatelessWidget {
       initialIndex: 0,
       length: 3,
       child: Scaffold(
-        appBar: PreferredSize(
-            preferredSize: Size(
-            double.infinity,
-            kToolbarHeight*2
+        appBar: AppBar(
+          titleSpacing: 0,
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          title: ShowUpTransition(
+            forward: true,
+            duration: Duration(milliseconds: 400),
+            slideSide: SlideFromSlide.TOP,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                margin: EdgeInsets.all(18),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 8),
+                      child: Icon(
+                        EvaIcons.musicOutline,
+                        color: Theme.of(context).accentColor,
+                      ),
+                    ),
+                    Text(
+                      "Media",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: "YTSans",
+                        color: Theme.of(context).textTheme.bodyText1.color
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10)
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12,
-                  offset: Offset(0.0, -2), //(x,y)
-                  blurRadius: 10.0,
-                  spreadRadius: 0.6
-                ),
-              ],
+          bottom: TabBar(
+            labelStyle: TextStyle(
+              fontFamily: 'Varela',
+              fontWeight: FontWeight.w500
             ),
-            child: AppBar(
-              titleSpacing: 0,
-              elevation: 0,
-              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-              title: ShowUpTransition(
-                forward: true,
-                duration: Duration(milliseconds: 400),
-                slideSide: SlideFromSlide.TOP,
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    margin: EdgeInsets.all(18),
-                    child: Row(
-                      children: [
-                        Container(
-                          margin: EdgeInsets.only(right: 8),
-                          child: Icon(
-                            EvaIcons.musicOutline,
-                            color: Theme.of(context).accentColor,
-                          ),
-                        ),
-                        Text(
-                          "Media",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontFamily: "YTSans",
-                            color: Theme.of(context).textTheme.bodyText1.color
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              bottom: TabBar(
-                labelStyle: TextStyle(
+            labelColor: Theme.of(context).textTheme.bodyText1.color,
+            unselectedLabelColor: Theme.of(context).textTheme.bodyText1.color,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorColor: Theme.of(context).accentColor,
+            tabs: [
+              Tab(child: Text(
+                "Downloads",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
                   fontFamily: 'Varela',
-                  fontWeight: FontWeight.w500
+                  color: Theme.of(context).iconTheme.color
                 ),
-                labelColor: Theme.of(context).textTheme.bodyText1.color,
-                unselectedLabelColor: Theme.of(context).textTheme.bodyText1.color,
-                indicatorSize: TabBarIndicatorSize.label,
-                indicatorColor: Theme.of(context).accentColor,
-                tabs: [
-                  Tab(child: Text(
-                    "Downloads",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontFamily: 'Varela',
-                      color: Theme.of(context).iconTheme.color
-                    ),
-                  )),
-                  Tab(child: Text(
-                    "Music",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontFamily: 'Varela',
-                      color: Theme.of(context).iconTheme.color
-                    ),
-                  )),
-                  Tab(child: Text(
-                    "Videos",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontFamily: 'Varela',
-                      color: Theme.of(context).iconTheme.color
-                    ),
-                  ))
-                ],
-              ),
-            ),
+              )),
+              Tab(child: Text(
+                "Music",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontFamily: 'Varela',
+                  color: Theme.of(context).iconTheme.color
+                ),
+              )),
+              Tab(child: Text(
+                "Videos",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontFamily: 'Varela',
+                  color: Theme.of(context).iconTheme.color
+                ),
+              ))
+            ],
           ),
         ),
         body: TabBarView(
