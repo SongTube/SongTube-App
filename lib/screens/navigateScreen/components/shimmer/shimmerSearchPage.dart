c940fd687b824b03deaea94bfc98103920569180
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-10-26 22:34:49 -0400

    Screens: Navigate: update ShimmerSearchPage

--- lib/screens/navigateScreen/components/shimmer/shimmerSearchPage.dart
+++ lib/screens/navigateScreen/components/shimmer/shimmerSearchPage.dart
@@ -12,22 +12,80 @@ class ShimmerSearchPage extends StatelessWidget {
         return ShowUpTransition(
           forward: true,
           duration: Duration(milliseconds: 400),
-          child: Shimmer.fromColors(
-            baseColor: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.2),
-            highlightColor: Theme.of(context).cardColor,
-            child: Container(
-              margin: EdgeInsets.only(
-                top: index == 0 ? 20 : 10,
-                bottom: index == 10 ? 20 : 10,
-                left: 20,
-                right: 20
-              ),
-              height: 100,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Theme.of(context).scaffoldBackgroundColor
-              ),
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: 16,
+              bottom: index == 11 ? 16 : 0
+            ),
+            child: Column(
+              children: [
+                AspectRatio(
+                  aspectRatio: 16/9,
+                  child: Shimmer.fromColors(
+                    baseColor: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.2),
+                    highlightColor: Theme.of(context).cardColor,
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        left: 12,
+                        right: 12,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Theme.of(context).scaffoldBackgroundColor
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 12, right: 12, top: 12, bottom: 4),
+                  child: Row(
+                    children: [
+                      Shimmer.fromColors(
+                        baseColor: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.2),
+                        highlightColor: Theme.of(context).cardColor,
+                        child: Container(
+                          height: 60,
+                          width: 60,
+                          margin: EdgeInsets.only(right: 8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            color: Theme.of(context).scaffoldBackgroundColor
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Shimmer.fromColors(
+                              baseColor: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.2),
+                              highlightColor: Theme.of(context).cardColor,
+                              child: Container(
+                                height: 20,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: Theme.of(context).scaffoldBackgroundColor
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Shimmer.fromColors(
+                              baseColor: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.2),
+                              highlightColor: Theme.of(context).cardColor,
+                              child: Container(
+                                height: 20,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: Theme.of(context).scaffoldBackgroundColor
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         );
