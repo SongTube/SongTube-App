5b347731bd1a8241811166a314be1b90b57b2631
Jesus Acosta <jesusgabriel.91@gmail.com>
2020-12-08 15:51:03 -0400

    DownloadMenu: automatically fetch StreamManifest if only URL is provided

--- lib/downloadMenu/downloadMenu.dart
+++ lib/downloadMenu/downloadMenu.dart
@@ -1,20 +1,24 @@
 // Flutter
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:songtube/downloadMenu/components/homeMenu.dart';
 
 // Internal
 import 'package:songtube/downloadMenu/components/videoMenu.dart';
 import 'package:songtube/downloadMenu/components/audioMenu.dart';
+import 'package:songtube/provider/managerProvider.dart';
 
 // Packages
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
-enum CurrentDownloadMenu { Home, Audio, Video }
+enum CurrentDownloadMenu { Home, Audio, Video, Loading }
 
 class DownloadMenu extends StatefulWidget {
   final StreamManifest streamManifest;
+  final String videoUrl;
   DownloadMenu({
-    @required this.streamManifest
+    @required this.streamManifest,
+    this.videoUrl
   });
   @override
   _DownloadMenuState createState() => _DownloadMenuState();
@@ -25,11 +29,34 @@ class _DownloadMenuState extends State<DownloadMenu> with TickerProviderStateMix
   // Current Download Menu Sub-Menu
   CurrentDownloadMenu currentDownloadMenu;
 
+  // Download Menu StreamManifest
+  StreamManifest manifest;
+
   @override
   void initState() {
     currentDownloadMenu =
-      CurrentDownloadMenu.Home;
+      CurrentDownloadMenu.Loading;
     super.initState();
+    if (widget.streamManifest == null) {
+      Provider.of<ManagerProvider>(context, listen: false)
+        .youtubeExtractor.getStreamManifest(
+          VideoId(VideoId.parseVideoId(widget.videoUrl))
+        ).then((value) {
+          manifest = value;
+          setState(() => currentDownloadMenu = CurrentDownloadMenu.Home);
+        });
+    } else {
+      manifest = widget.streamManifest;
+      setState(() => currentDownloadMenu = CurrentDownloadMenu.Home);
+    }
+    
+  }
+
+  @override
+  void dispose() {
+    Provider.of<ManagerProvider>(context, listen: false)
+      .youtubeExtractor.killIsolates();
+    super.dispose();
   }
 
   Widget build(BuildContext context) {
@@ -56,7 +83,7 @@ class _DownloadMenuState extends State<DownloadMenu> with TickerProviderStateMix
       case CurrentDownloadMenu.Audio:
         returnWidget = Container(
           child: AudioDownloadMenu(
-            audioList: widget.streamManifest.audioOnly
+            audioList: manifest.audioOnly
               .sortByBitrate()
               .reversed.toList(),
             onBack: () => setState(() => 
@@ -68,14 +95,23 @@ class _DownloadMenuState extends State<DownloadMenu> with TickerProviderStateMix
         returnWidget = Container(
           height: MediaQuery.of(context).size.height*0.6,
           child: VideoDownloadMenu(
-            videoList: widget.streamManifest.videoOnly
+            videoList: manifest.videoOnly
               .sortByVideoQuality(),
             onOptionSelect: (list) => Navigator.pop(context, list),
-            audioSize: widget.streamManifest.audioOnly
+            audioSize: manifest.audioOnly
               .withHighestBitrate()
               .size.totalMegaBytes,
             onBack: () => setState(() => 
               currentDownloadMenu = CurrentDownloadMenu.Home),
+          ),
+        ); break;
+      case CurrentDownloadMenu.Loading:
+        returnWidget = Container(
+          height: MediaQuery.of(context).size.height*0.6,
+          child: Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(Theme.of(context).accentColor),
+            ),
           ),
         ); break;
     }
