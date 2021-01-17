5f6823f3796a70403fe031afea38f1e8872ac2dc
Jesus Acosta <jesusgabriel.91@gmail.com>
2021-01-17 12:18:15 -0400

    ui: autohideScaffold: add "floatingWidget" implementation

--- lib/ui/components/autohideScaffold.dart
+++ lib/ui/components/autohideScaffold.dart
@@ -5,24 +5,29 @@ class AutoHideScaffold extends StatefulWidget {
   final Widget appBar;
   final Widget body;
   final Widget bottomNavigationBar;
+  final Widget floatingWidget;
+  final Function(double) floatingWidgetPositionCallback;
   final bool resizeToAvoidBottomInset;
-  final Key key;
   final Color backgroundColor;
   AutoHideScaffold({
     this.appBar,
     @required this.body,
     this.bottomNavigationBar,
+    this.floatingWidget,
     this.resizeToAvoidBottomInset,
+    this.floatingWidgetPositionCallback,
     this.backgroundColor,
-    this.key
+    Key key
   }) : super(key: key);
+
   @override
-  _AutoHideScaffoldState createState() => _AutoHideScaffoldState();
+  AutoHideScaffoldState createState() => AutoHideScaffoldState();
 }
 
-class _AutoHideScaffoldState extends State<AutoHideScaffold> with TickerProviderStateMixin {
+class AutoHideScaffoldState extends State<AutoHideScaffold> with TickerProviderStateMixin {
 
   AnimationController _hideAnimationController;
+  bool navigationBarScrolledDown = false;
 
   @override
   void initState() {
@@ -33,36 +38,67 @@ class _AutoHideScaffoldState extends State<AutoHideScaffold> with TickerProvider
     super.initState();
   }
 
+  /// For any implementation of the [floatingWidget] made, you will need
+  /// to add a GlobaKey<AutoHideScaffoldState> and use it to update the AppBar/NavBar
+  /// position if you need them to hide them (ex: [floatingWidget] is a slidable panel
+  /// and you want to hide the appbar and navbar on panel slide)
+  void updateInternalController(double position) {
+    if (!navigationBarScrolledDown) {
+      _hideAnimationController.animateTo((1 - position));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
       animation: _hideAnimationController,
       builder: (context, child) {
-        return Scaffold(
-          backgroundColor: widget.backgroundColor,
-          resizeToAvoidBottomInset: widget.resizeToAvoidBottomInset,
-          appBar: widget.appBar != null ? PreferredSize(
-            preferredSize: Size(
-              double.infinity,
-              kToolbarHeight * _hideAnimationController.value
+        return Stack(
+          children: [
+            Scaffold(
+              backgroundColor: widget.backgroundColor,
+              resizeToAvoidBottomInset: widget.resizeToAvoidBottomInset,
+              appBar: widget.appBar != null ? PreferredSize(
+                preferredSize: Size(
+                  double.infinity,
+                  kToolbarHeight * _hideAnimationController.value
+                ),
+                child: widget.appBar,
+              ): null,
+              body: child,
+              bottomNavigationBar: widget.bottomNavigationBar != null ? Container(
+                width: double.infinity,
+                height: (kBottomNavigationBarHeight) *
+                  _hideAnimationController.value,
+                child: SingleChildScrollView(
+                  physics: NeverScrollableScrollPhysics(),
+                  child: widget.bottomNavigationBar
+                ),
+              ) : null,
             ),
-            child: widget.appBar,
-          ): null,
-          body: child,
-          bottomNavigationBar: widget.bottomNavigationBar != null ? Container(
-            width: double.infinity,
-            height: kBottomNavigationBarHeight *
-              _hideAnimationController.value,
-            child: SingleChildScrollView(
-              physics: NeverScrollableScrollPhysics(),
-              child: widget.bottomNavigationBar,
-            ),
-          ) : null,
+            AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              child: widget.floatingWidget != null
+                ? Padding(
+                    padding: EdgeInsets.only(
+                      bottom: kBottomNavigationBarHeight *
+                        _hideAnimationController.value
+                    ),
+                    child: widget.floatingWidget
+                ) : Container()
+            )
+          ],
         );
       },
       child: ScrollDetector(
-        onScrollDown: () => _hideAnimationController.reverse(),
-        onScrollUp: () => _hideAnimationController.forward(),
+        onScrollDown: () {
+          _hideAnimationController.reverse();
+          navigationBarScrolledDown = true;
+        },
+        onScrollUp: () {
+          _hideAnimationController.forward();
+          navigationBarScrolledDown = false;
+        },
         child: widget.body
       ),
     );
