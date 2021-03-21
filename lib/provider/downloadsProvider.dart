6eb0b7b58ac0dac69cc8ccc76537fe3d020c3a04
Jesus Acosta <jesusgabriel.91@gmail.com>
2021-03-21 16:37:34 -0400

    provider: downloadsProvider: update to match recent changes to download models

--- lib/provider/downloadsProvider.dart
+++ lib/provider/downloadsProvider.dart
@@ -5,92 +5,47 @@ import 'package:newpipeextractor_dart/models/streams/videoOnlyStream.dart';
 import 'package:newpipeextractor_dart/models/video.dart';
 import 'package:songtube/internal/ffmpeg/converter.dart';
 import 'package:songtube/internal/languages.dart';
-import 'package:songtube/internal/models/audioModifiers.dart';
-import 'package:songtube/internal/models/infoSets/downloadinfoset.dart';
-import 'package:songtube/internal/models/metadata.dart';
+import 'package:songtube/internal/download/audioFilters.dart';
+import 'package:songtube/internal/download/downloadItem.dart';
+import 'package:songtube/internal/download/downloadSet.dart';
+import 'package:songtube/internal/download/tags.dart';
 import 'package:songtube/internal/randomString.dart';
 import 'package:songtube/provider/configurationProvider.dart';
 
 class DownloadsProvider extends ChangeNotifier {
 
   DownloadsProvider() {
-    queueList       = <DownloadInfoSet>[];
-    downloadingList = <DownloadInfoSet>[];
-    convertingList  = <DownloadInfoSet>[];
-    completedList   = <DownloadInfoSet>[];
-    cancelledList   = <DownloadInfoSet>[];
+    queueList       = <DownloadSet>[];
+    downloadingList = <DownloadSet>[];
+    convertingList  = <DownloadSet>[];
+    completedList   = <DownloadSet>[];
+    cancelledList   = <DownloadSet>[];
     
   }
 
   // Queue List
-  List<DownloadInfoSet> queueList;
+  List<DownloadSet> queueList;
 
   // Downloading List
-  List<DownloadInfoSet> downloadingList;
+  List<DownloadSet> downloadingList;
 
   // Converting List
-  List<DownloadInfoSet> convertingList;
+  List<DownloadSet> convertingList;
 
   // Completed List
-  List<DownloadInfoSet> completedList;
+  List<DownloadSet> completedList;
 
   // Cancelled List
-  List<DownloadInfoSet> cancelledList;
+  List<DownloadSet> cancelledList;
 
   // Handle Single Video Download
-  void handleVideoDownload({
+  void handleDownloadItem({
     @required Languages language,
-    ConfigurationProvider config,
-    DownloadMetaData metadata,
-    YoutubeVideo videoDetails,
-    List data,
+    DownloadItem item
   }) {
-    DownloadType downloadType;
-    FFmpegActionType convertFormat;
-    if (config.ffmpegActionTypeFormat == "AAC")
-      convertFormat = FFmpegActionType.ConvertToAAC;
-    if (config.ffmpegActionTypeFormat == "OGG Vorbis")
-      convertFormat = FFmpegActionType.ConvertToOGGVorbis;
-    if (config.ffmpegActionTypeFormat == "MP3")
-      convertFormat = FFmpegActionType.ConvertToMP3;
-    if (config.enableFFmpegActionType == false)
-      convertFormat = FFmpegActionType.NONE;
-    String downloadPath;
-    AudioOnlyStream audioStreamInfo;
-    VideoOnlyStream videoStreamInfo;
-    switch (data[0]) {
-      case "Audio":
-        downloadType = DownloadType.AUDIO;
-        downloadPath = config.audioDownloadPath;
-        audioStreamInfo = data[1];
-        break;
-      case "Video":
-        downloadType = DownloadType.VIDEO;
-        videoStreamInfo = data[1];
-        audioStreamInfo = videoDetails
-          .getAudioStreamWithBestMatchForVideoStream(videoStreamInfo);
-        downloadPath = config.videoDownloadPath;
-        convertFormat = FFmpegActionType.AppendAudioOnVideo;
-        break;
-    }
-    metadata.title = removeToxicSymbols(metadata.title);
-    DownloadInfoSet download = new DownloadInfoSet(
+    DownloadSet download = new DownloadSet(
       language: language,
-      audioStreamInfo: audioStreamInfo,
-      videoStreamInfo: videoStreamInfo,
-      videoDetails: videoDetails.toStreamInfoItem(),
-      metadata: metadata,
-      downloadType: downloadType,
-      downloadPath: config.enableAlbumFolder
-        ? downloadPath + "/${metadata.album}"
-        : downloadPath,
-      convertFormat: convertFormat,
-      audioModifiers: AudioModifiers(
-        volume: double.parse(data[2]),
-        bassGain: int.parse(data[3]),
-        trebleGain: int.parse(data[4])
-      ),
-      normalizeAudio: data[5],
+      downloadItem: item,
       downloadId: RandomString.getRandomString(6),
       convertingCallback: (String downloadId) {
         moveToConverting(downloadId);
@@ -110,66 +65,9 @@ class DownloadsProvider extends ChangeNotifier {
     checkQueue();
   }
 
-  // Handle Playlist Downloads
-  void handlePlaylistDownload({
-    @required Languages language,
-    ConfigurationProvider config,
-    List<StreamInfoItem> listVideos,
-    String album, String artist
-  }) {
-    int track = 1;
-    FFmpegActionType convertFormat;
-    if (config.ffmpegActionTypeFormat == "AAC")
-      convertFormat = FFmpegActionType.ConvertToAAC;
-    if (config.ffmpegActionTypeFormat == "OGG Vorbis")
-      convertFormat = FFmpegActionType.ConvertToOGGVorbis;
-    if (config.ffmpegActionTypeFormat == "MP3")
-      convertFormat = FFmpegActionType.ConvertToMP3;
-    if (config.enableFFmpegActionType == false)
-      convertFormat = FFmpegActionType.NONE;
-    listVideos.forEach((infoItem) {
-      queueList.add(
-        new DownloadInfoSet(
-          language: language,
-          audioStreamInfo: null,
-          videoDetails: infoItem,
-          metadata: DownloadMetaData(
-            title: removeToxicSymbols(infoItem.name),
-            album: album, artist: artist,
-            genre: "Any", coverurl: infoItem.thumbnails.hqdefault,
-            date: infoItem.uploadDate,
-            disc: "1", track: "$track"
-          ),
-          downloadType: DownloadType.AUDIO,
-          downloadPath: config.enableAlbumFolder
-            ? config.audioDownloadPath + "/$album"
-            : config.audioDownloadPath,
-          convertFormat: convertFormat,
-          audioModifiers: AudioModifiers(),
-          downloadId: RandomString.getRandomString(6),
-          convertingCallback: (String downloadId) {
-            moveToConverting(downloadId);
-          },
-          completedCallback: (String downloadId, bool converted) {
-            moveToCompleted(downloadId, converted);
-            checkQueue();
-          },
-          cancelledCallback: (String downloadId) {
-            moveToCancelled(downloadId);
-          },
-          saveErrorCallback: (String downloadId) {
-            moveToCancelled(downloadId);
-          }
-        ),
-      );
-      track++;
-    });
-    checkQueue();
-  }
-
   void checkQueue() {
     if (queueList.isNotEmpty && downloadingList.length < 2) {
-      DownloadInfoSet download = queueList[0];
+      DownloadSet download = queueList[0];
       downloadingList.add(download);
       int index = downloadingList.indexWhere((element)
         => element.downloadId == download.downloadId);
@@ -229,16 +127,5 @@ class DownloadsProvider extends ChangeNotifier {
     checkQueue();
   }
 
-  String removeToxicSymbols(String string) {
-    return string
-      .replaceAll('Container.', '')
-      .replaceAll(r'\', '')
-      .replaceAll('/', '')
-      .replaceAll('*', '')
-      .replaceAll('?', '')
-      .replaceAll('"', '')
-      .replaceAll('<', '')
-      .replaceAll('>', '')
-      .replaceAll('|', '');
-  }
+  
 }
