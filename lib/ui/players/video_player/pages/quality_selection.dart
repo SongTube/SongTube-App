f7ba94dbc18bf8bb5557be8e93839cfd5f126a95
Jesus Acosta <jesusgabriel.91@gmail.com>
2023-12-01 17:07:48 -0400

    adjust video options menu UI

--- lib/ui/players/video_player/pages/quality_selection.dart
+++ lib/ui/players/video_player/pages/quality_selection.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
+import 'package:songtube/internal/models/colors_palette.dart';
 import 'package:songtube/internal/models/content_wrapper.dart';
 import 'package:songtube/internal/models/playback_quality.dart';
 import 'package:songtube/languages/languages.dart';
+import 'package:songtube/providers/media_provider.dart';
+import 'package:songtube/ui/animations/animated_icon.dart';
+import 'package:songtube/ui/animations/animated_text.dart';
 import 'package:songtube/ui/sheet_phill.dart';
+import 'package:songtube/ui/sheets/common_sheet.dart';
 import 'package:songtube/ui/text_styles.dart';
 
 class PlaybackQualitySheet extends StatefulWidget {
@@ -32,93 +39,117 @@ class _PlaybackQualitySheetState extends State<PlaybackQualitySheet> {
   final List<double> values = [0.25, 0.5, 0.75, 1.0, 1.25, 1.50, 1.75, 2.0];
   late int playbackSpeedIndex = values.indexWhere((element) => element == widget.currentPlaybackSpeed);
 
-  SliderThemeData sliderTheme() {
+  SliderThemeData sliderTheme(ColorsPalette colors) {
     return SliderThemeData(
-      thumbColor: Theme.of(context).primaryColor,
-      activeTrackColor: Theme.of(context).primaryColor,
-      trackHeight: 6,
+      thumbColor: colors.vibrant,
+      activeTrackColor: colors.vibrant,
+      trackHeight: 4,
       thumbShape: const RoundSliderThumbShape(
-        disabledThumbRadius: 7,
-        enabledThumbRadius: 7
+        disabledThumbRadius: 5,
+        enabledThumbRadius: 5
       ),
-      inactiveTickMarkColor: Theme.of(context).primaryColor,
-      inactiveTrackColor: Theme.of(context).scaffoldBackgroundColor.withOpacity(0.6),
+      inactiveTickMarkColor: colors.vibrant,
+      inactiveTrackColor: Theme.of(context).scaffoldBackgroundColor,
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).cardColor,
-        borderRadius: BorderRadius.circular(20)
-      ),
-      margin: const EdgeInsets.all(12),
-      padding: const EdgeInsets.only(top: 16, left: 16),
-      child: Wrap(
-        children: [
-          const Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: EdgeInsets.only(right: 16),
-              child: BottomSheetPhill(),
-            )),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 12),
-              // Video playback speed
-              Text(Languages.of(context)!.labelPlaybackSpeed, style: textStyle(context)),
-              const SizedBox(height: 6),
-              Row(
+    MediaProvider mediaProvider = Provider.of(context);
+    return CommonSheet(
+      useCustomScroll: false,
+      builder: (context, scrollController) {
+        return Wrap(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 12, right: 12),
+              child: Row(
                 children: [
-                  Text('${Languages.of(context)!.labelCurrent}: ', style: subtitleTextStyle(context, opacity: 0.7)),
-                  Text('x${values[playbackSpeedIndex].toStringAsFixed(2)}', style: subtitleTextStyle(context).copyWith(color: Theme.of(context).primaryColor)),
-                ],
-              ),
-              Row(
-                children: [
-                  Text('0.25', style: smallTextStyle(context, opacity: 0.7)),
-                  Expanded(
-                    child: SliderTheme(
-                      data: sliderTheme(),
-                      child: Slider(
-                        value: playbackSpeedIndex.toDouble(),
-                        min: 0,
-                        max: values.length - 1,
-                        divisions: values.length - 1,
-                        onChanged: (index) {
-                          setState(() {
-                            playbackSpeedIndex = index.toInt();
-                          });
-                        },
-                        onChangeEnd: (index) {
-                          widget.onPlaybackSpeedChange(values[index.toInt()]);
-                        },
-                      )
-                    ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: const AppAnimatedIcon(Iconsax.arrow_left, size: 22)
                   ),
-                  Text('2.00', style: smallTextStyle(context, opacity: 0.7)),
-                  const SizedBox(width: 16),
+                  const SizedBox(width: 12),
+                  Expanded(child: Text(Languages.of(context)!.labelPlayerSettings, style: textStyle(context, bold: false))),
                 ],
               ),
-              // Video current quality
-              Text(Languages.of(context)!.labelCurrentQuality, style: textStyle(context)),
-              const SizedBox(height: 6),
-              Text('${Languages.of(context)!.labelFastStreamingOptions}:', style: subtitleTextStyle(context, opacity: 0.7)),
-              Padding(
-                padding: const EdgeInsets.only(top: 12, bottom: 12),
-                child: videoOptions(),
-              ),
-              Text('${Languages.of(context)!.labelStreamingOptions}:', style: subtitleTextStyle(context, opacity: 0.7)),
-              Padding(
-                padding: const EdgeInsets.only(top: 12, bottom: 12),
-                child: videoOnlyOptions(),
-              )
-            ],
-          ),
-        ],
-      ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 12),
+                // Video playback speed
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, right: 12),
+                  child: Text(Languages.of(context)!.labelPlaybackSpeed, style: subtitleTextStyle(context)),
+                ),
+                const SizedBox(height: 2),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, right: 12),
+                  child: Row(
+                    children: [
+                      Text('${Languages.of(context)!.labelCurrent}: ', style: subtitleTextStyle(context, opacity: 0.6).copyWith(fontSize: 14)),
+                      AnimatedText('x${values[playbackSpeedIndex].toStringAsFixed(2)}', style: subtitleTextStyle(context).copyWith(letterSpacing: 1), auto: true),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, right: 12),
+                  child: Row(
+                    children: [
+                      Text('0.25', style: tinyTextStyle(context, opacity: 0.6).copyWith(letterSpacing: 1)),
+                      Expanded(
+                        child: SliderTheme(
+                          data: sliderTheme(mediaProvider.currentColors),
+                          child: Slider(
+                            value: playbackSpeedIndex.toDouble(),
+                            min: 0,
+                            max: values.length - 1,
+                            divisions: values.length - 1,
+                            onChanged: (index) {
+                              setState(() {
+                                playbackSpeedIndex = index.toInt();
+                              });
+                            },
+                            onChangeEnd: (index) {
+                              widget.onPlaybackSpeedChange(values[index.toInt()]);
+                            },
+                          )
+                        ),
+                      ),
+                      Text('2.00', style: tinyTextStyle(context, opacity: 0.6).copyWith(letterSpacing: 1)),
+                    ],
+                  ),
+                ),
+                // Video current quality
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, right: 12),
+                  child: Text(Languages.of(context)!.labelCurrentQuality, style: subtitleTextStyle(context)),
+                ),
+                const SizedBox(height: 2),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, right: 12),
+                  child: Text('${Languages.of(context)!.labelFastStreamingOptions}:', style: subtitleTextStyle(context, opacity: 0.6).copyWith(fontSize: 14)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12, bottom: 16),
+                  child: videoOptions(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, right: 12),
+                  child: Text('${Languages.of(context)!.labelStreamingOptions}:', style: subtitleTextStyle(context, opacity: 0.6).copyWith(fontSize: 14)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12, bottom: 16),
+                  child: videoOnlyOptions(),
+                )
+              ],
+            ),
+          ],
+        );
+      }
     );
   }
 
@@ -126,10 +157,9 @@ class _PlaybackQualitySheetState extends State<PlaybackQualitySheet> {
     return SizedBox(
       height: kToolbarHeight,
       child: ListView.builder(
-        padding: const EdgeInsets.all(0),
+        padding: const EdgeInsets.only(left: 16, right: 16),
         shrinkWrap: true,
         scrollDirection: Axis.horizontal,
-        
         itemCount: videoList.length,
         itemBuilder: (context, index) {
           return optionTile(videoList[index]);
@@ -142,10 +172,9 @@ class _PlaybackQualitySheetState extends State<PlaybackQualitySheet> {
     return SizedBox(
       height: kToolbarHeight,
       child: ListView.builder(
-        padding: const EdgeInsets.all(0),
+        padding: const EdgeInsets.only(left: 16, right: 16),
         shrinkWrap: true,
         scrollDirection: Axis.horizontal,
-        
         itemCount: videoOnlyList.length,
         itemBuilder: (context, index) {
           return optionTile(videoOnlyList[index]);
@@ -155,6 +184,7 @@ class _PlaybackQualitySheetState extends State<PlaybackQualitySheet> {
   }
 
   Widget optionTile(VideoPlaybackQuality quality) {
+    MediaProvider mediaProvider = Provider.of(context);
     final selected = widget.currentQuality == quality;
     return GestureDetector(
       onTap: () {
@@ -162,18 +192,17 @@ class _PlaybackQualitySheetState extends State<PlaybackQualitySheet> {
         Navigator.pop(context);
       },
       child: Container(
-        padding: const EdgeInsets.all(8).copyWith(left: 16, right: 16),
+        padding: const EdgeInsets.all(0).copyWith(left: 24, right: 24),
         margin: const EdgeInsets.only(right: 12),
         decoration: BoxDecoration(
-          color: selected ? Theme.of(context).primaryColor : Theme.of(context).scaffoldBackgroundColor,
+          color: selected ? mediaProvider.currentColors.vibrant?.withOpacity(0.07) : Theme.of(context).scaffoldBackgroundColor,
           borderRadius: BorderRadius.circular(15),
-          border: Border.all(color: Theme.of(context).dividerColor.withOpacity(0.06), width: 1.5)
         ),
         child: Row(
           children: [
-            Text('${quality.resolution}p', style: subtitleTextStyle(context, bold: selected ? true : false)),
+            AnimatedText('${quality.resolution}p', style: subtitleTextStyle(context, opacity: 0.6).copyWith(fontSize: 14), auto: selected, opacity: selected ? 1 : 0.6),
             if (quality.framerate > 30)
-            Text(' ${quality.framerate.round()}FPS', style: smallTextStyle(context, bold: true).copyWith(color: Theme.of(context).primaryColor))
+            AnimatedText(' ${quality.framerate.round()}FPS', style: smallTextStyle(context).copyWith(fontSize: 14), auto: true)
           ],
         ),
       ),
